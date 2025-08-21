bc413357c811f62572524874d3173e11a09f96c5
Swapnajeet <swapnajeetbhandare5@gmail.com>
2025-08-21 16:14:03 +0530

    first file of angular

---
+++ bind.js
@@ -0,0 +1,10 @@
+var app = angular.module("studentApp",[]);
+
+app.controller("studentcontroller",function($scope){
+   $scope.student ={
+    name:"swapanjeet",
+    roll:"2002",
+    course:"computer science"
+   };
+});
+
